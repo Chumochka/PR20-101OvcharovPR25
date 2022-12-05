b53196b8cfd1350cf3b1239fc059a61e1816c404
Chumochka <danpro1010@bk.ru>
2022-12-05 20:01:23 +0700

    2

--- app/src/main/java/com/example/pr25/MyService.java
+++ app/src/main/java/com/example/pr25/MyService.java
@@ -4,9 +4,13 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.concurrent.TimeUnit;
 
-public class MyService extends Service {
+public class MyService extends Service { //ServiceSimple
 
     final String LOG_TAG = "myLogs";
 
@@ -17,12 +21,14 @@ public class MyService extends Service {
 
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "onStartCommand");
+        Toast.makeText(getApplicationContext(), "Сервис1 запущен", Toast.LENGTH_LONG).show();
         someTask();
         return super.onStartCommand(intent, flags, startId);
     }
 
     public void onDestroy() {
         super.onDestroy();
+        Toast.makeText(getApplicationContext(), "Сервис1 уничтожен", Toast.LENGTH_LONG).show();
         Log.d(LOG_TAG, "onDestroy");
     }
 
@@ -35,6 +41,7 @@ public class MyService extends Service {
         new Thread(new Runnable() {
             public void run() {
                 for (int i = 1; i<=5; i++) {
+                    String msg = "i" + i;
                     Log.d(LOG_TAG, "i = " + i);
                     try {
                         TimeUnit.SECONDS.sleep(1);
